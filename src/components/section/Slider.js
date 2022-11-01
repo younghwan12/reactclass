99bf25330754b539ac365568b53ff1724d9bc5ba
younghwan12 <dowk3432@gmail.com>
2022-11-01 18:50:17 +0900

    2022.11.01

--- src/components/section/Slider.js
+++ src/components/section/Slider.js
@@ -13,7 +13,6 @@ const sliderText = [
     desc1: "VARIETY OF DRINK",
     desc2: "세상에 아주 다양한 술들이 있습니다.",
     desc3: "지금 만나러 가시죠",
-    address: "/",
   },
   {
     imgClass: "img2",
@@ -22,7 +21,6 @@ const sliderText = [
     desc1: "VARIETY OF DRINK",
     desc2: "세상에 아주 다양한 술들이 있습니다.",
     desc3: "지금 만나러 가시죠",
-    address: "/",
   },
   {
     imgClass: "img3",
@@ -31,34 +29,8 @@ const sliderText = [
     desc1: "VARIETY OF DRINK",
     desc2: "세상에 아주 다양한 술들이 있습니다.",
     desc3: "지금 만나러 가시죠",
-    address: "/",
   },
 ];
-
-const SliderText = ({ imgClass, cate, title, desc1, desc2, desc3 }) => {
-  return (
-    <SwiperSlide>
-      <div className={`swiper-slide ${imgClass}`}>
-        <div className="desc">
-          <span>{cate}</span>
-          <h3>{title}</h3>
-          <p>
-            {desc1}
-            <br />
-            {desc2}
-            <br /> {desc3}
-          </p>
-          <div className="btn">
-            <a href="/">자세히 보기</a>
-            <a href="/" className="black">
-              사이트 보기
-            </a>
-          </div>
-        </div>
-      </div>
-    </SwiperSlide>
-  );
-};
 
 function Slider(props) {
   return (
@@ -70,17 +42,7 @@ function Slider(props) {
         modules={[Autoplay, Navigation, Pagination]}
         className="mySwiper"
       >
-        {sliderText.map((slider) => (
-          <SliderText
-            imgClass={slider.imgClass}
-            cate={slider.cate}
-            title={slider.title}
-            desc1={slider.desc1}
-            desc2={slider.desc2}
-            desc3={slider.desc3}
-          />
-        ))}
-        {/* <SwiperSlide>
+        <SwiperSlide>
           <div className="swiper-slide img1">
             <div className="desc">
               <span>{sliderText.cate}</span>
@@ -99,7 +61,47 @@ function Slider(props) {
               </div>
             </div>
           </div>
-        </SwiperSlide> */}
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="swiper-slide img2">
+            <div className="desc">
+              <span>{sliderText.cate}</span>
+              <h3>{sliderText.title}</h3>
+              <p>
+                {sliderText.desc1}
+                <br />
+                {sliderText.desc2}
+                <br /> {sliderText.desc3}
+              </p>
+              <div className="btn">
+                <a href="/">자세히 보기</a>
+                <a href="/" className="black">
+                  사이트 보기
+                </a>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="swiper-slide img3">
+            <div className="desc">
+              <span>{sliderText.cate}</span>
+              <h3>{sliderText.title}</h3>
+              <p>
+                {sliderText.desc1}
+                <br />
+                {sliderText.desc2}
+                <br /> {sliderText.desc3}
+              </p>
+              <div className="btn">
+                <a href="/">자세히 보기</a>
+                <a href="/" className="black">
+                  사이트 보기
+                </a>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
